b3988a88c6d99d1de4af8442577acb053ec922c3
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-11-16 23:23:15 -0500

    assignment complete

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:" 
+    katz_deli.each.with_index(1) do |name, i| 
+      current_line << " #{i}. #{name}"
+    end
+    puts current_line
+  end
+end
+
+def take_a_number(katz_deli, name_joined_line)
+  katz_deli << name_joined_line
+  puts "Welcome, #{name_joined_line}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.first}."
+    katz_deli.shift
+  end
+end
